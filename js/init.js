6fae0211391d5fbbb9817c2aeb69ee25b38adb12
yohniF <yohni.123@gmail.com>
2017-09-26 22:20:09 +0700

    upload

---
+++ js/init.js
@@ -0,0 +1,19 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+
+  }); 
+    
+    
+     $(document).ready(function(){
+      $('.carousel').carousel();
+    });
+    
+    
+    
+    
+    
+    // end of document ready
+})(jQuery); // end of jQuery name space
